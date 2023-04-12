d98711152ad5a4da2b22afbb89694ad0a2efe5a9
DEVP-CHANDAN <96979992+DEVP-CHANDAN@users.noreply.github.com>
2023-04-12 21:36:50 +0530

    Update test1.java

--- test/src/test/test1.java
+++ test/src/test/test1.java
@@ -1,7 +1,37 @@
 package test;
 
-public class test1 {
-  public static void main(String[] args) {
-	System.out.println("CCSS1");
+class Parent{
+	int a;
+    Parent(int a){
+    	this.a = a;
+    }
+	public void print() {
+		System.out.println(this.a);
+	} 
+	
+	public void print(int a , int b) {
+		System.out.println(a+ " " + b);
+	}
 }
+
+class Child extends Parent{
+	Child(int a) {
+		super(a);
+	}
+
+	@Override
+	public void print(int a , int c) {
+		int b = a+10;
+		System.out.println(b+c);
+	}
+} 
+
+public class test1 {
+    public static void main(String[] args) {
+		Parent obj = new Parent(9);
+		obj.print();
+		obj.print(7,7);
+		Child obj1 = new Child(2);
+		obj1.print(4,3);
+	}
 }
